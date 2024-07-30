29ae83ece9f502235d0bcb687eb9ceedbf6e57b2
www3drbiz <www3drbiz@gmail>
2024-07-30 13:53:25 +0900

    Update_test_html.yml 추가

--- update_time.py
+++ update_time.py
@@ -4,5 +4,4 @@ now = datetime.datetime.now()
 current_time = now.strftime("%Y-%m-%d %H:%M:%S") 
 
 with open("index.html", "w") as f:
-    f.write(f"Last updated at (current_time")
-    
+    f.write(f"Last updated at (current_time)")
